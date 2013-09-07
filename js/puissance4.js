c709a0084b1c3e28db39499bbc938a5356a8fe3f
vava <marc.laville@polinux.net>
2013-09-08 01:57:06 +0200

    Premiere Version Jouable

--- js/puissance4.js
+++ js/puissance4.js
@@ -1,64 +1,126 @@
-// jeu : new Firebase('https://polinux-chrono.firebaseio.com/jeu/puissance4'),
-
-
-var appPuissance4 = (function ( url ) {
+/**
+ * puissance4.js
+ * * 
+ * @auteur     marc laville
+ * @Copyleft 2013
+ * @date       07/09/2013
+ * @version    0.1
+ * @revision   $0$
+ *
+ * Puissance 4
+ *
+ * A Faire :
+ * - standardisation du code + commentaire
+ * - Utiliser les transaction pour les entrées dans la partie https://www.firebase.com/docs/transactions.html
+ * - listener sur les deconnexion
+ * 
+ * Licensed under the GPL license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+var appPuissance4 = (function ( url, document ) {
 
   var jeu = new Firebase(url),
-	lancePartie = function(v) {
-		numPartie = jeu.child(('000000' + nPartie).slice(-6)).child('partie');
-	},
-	numPartie = 0;
+	frmAffichage = document.forms['affichage'],
+	colJeu = document.querySelectorAll('ol>li'),
+	partie = null,
+	joueur = 0,
+	aMoi = true,
+	couleur = 'rouge',
+	increment = function(nb) { return nb + 1; },
 
-  // A private function which logs any arguments
-  myPrivateMethod = function( foo ) {
-      console.log( foo );
-  };
-  
-	return {
-
-		// A public variable
-		myPublicVar: "foo",
-		
-		init: function( ) {
-//			numPartie = jeu.child('numPartie');
+	// Pose la candidature pour la partie dont le numero est renvoyée
+	afficheCoup = function(snapCoup) {
+		var coup = snapCoup.val();
+			col = coup.colonne;
 			
-			jeu.child('numPartie').once('value', function (snapshot) {
-				"use strict";
-				return lancePartie(snapshot.val()); 
-			});
-			return;
-		},
+		if(col != undefined) {
+			var cell = colJeu[col - 1].querySelector('li:last-child');
 
-		// A public function utilizing privates
-		myPublicFunction: function( bar ) {
-
-		  // Increment our private counter
-		  myPrivateVar++;
-
-		  // Call our private method using bar
-		  myPrivateMethod( bar );
-
+			while(cell) {
+				if(cell.className == '') {
+					cell.classList.add(coup.couleur);
+					cell = null;
+				} else {
+					cell = cell.previousSibling;
+				}
+			}
+			aMoi = !aMoi;
 		}
-	  };
-  
-})('https://polinux-chrono.firebaseio.com/jeu/puissance4');
-
-var np = appPuissance4.init();
-/*
-var appPuissance4 = {
-
-	jeu : new Firebase('https://polinux-chrono.firebaseio.com/jeu/puissance4'),
-	
-//	numPartie : appPuissance4.jeu.child('numPartie'),
-	numPartie : 0,
-
-	myProperty: "someValue",
-
-	myConfig: {
-		useCaching: true,
-		language: "en"
+		
+		return;
 	}
+	// Pose la candidature pour la partie dont le numero est renvoyée
+	candidatePartie = function(snapNum) {
+		// Affiche le numero de partie et incremente le nb de joueur
+		frmAffichage.numPartie.value = ('000000' + snapNum.val()).slice(-6);
+		partie = jeu.child(frmAffichage.numPartie.value).child('partie');
+		
+		// increment le nb de joueur
+		partie.child('nbJoueurs').transaction(increment,
+			function(error, committed, snapshot) {
+			  if (error)
+				alert('nbJoueurs : Transaction failed abnormally !', error);
+			  else if (!committed)
+				alert('Transaction aborted !');
+			  else {
+				var nbJoueur = 0 + snapshot.val();
+				
+				if (2 >= nbJoueur) {
+				
+					joueur = nbJoueur;
+					document.getElementById('nbJoueur').textContent = (joueur == 1) ? 'A' : 'B';
+					
+					if (1 == joueur) {
+						couleur = 'rouge';
+						// Attente du deuxieme joueur
+						document.getElementById('attente').textContent = 'attente joueur ';
+						partie.child( 'nbJoueurs' ).once( 'value', function(snapshot) {
+						
+							if(2 == 0 + snapshot.val())
+								document.getElementById('attente').textContent = '2eme joueur OK';
+						});
+					} else {
+						couleur = 'jaune';
+						aMoi = false;
+						document.getElementById('attente').textContent = '1er joueur OK';
+					}
+				
+					partie.on('child_added', afficheCoup);
+				} else {
+					// Création d'une nouvelle partie
+					document.getElementById('numPartie').textContent = 'Nouvelle Partie ';
+					jeu.child('numPartie').transaction(increment,
+						function(error, committed, snapshot) {
+						  if (error)
+							alert('numPartie : Transaction failed abnormally !', error);
+						  else if (!committed)
+							alert('Transaction aborted !');
+						  else
+							candidatePartie(snapshot);
+					});
+
+				}
+			  }
+		});
+	}
+
+	return {
+		// Lorsque l'on récupère le numero de la dernière partie, on candidate
+		init: function( ) { return jeu.child('numPartie').once('value', candidatePartie); },
+
+		joue: function(e) {
+			if(aMoi) {
+				partie.push({ couleur: couleur, colonne: e.target.parentNode.dataset.col });
+			}
+		}
+	};
+  
+})('https://polinux-chrono.firebaseio.com/jeu/puissance4', window.document);
+
+var bts = document.querySelectorAll('ol li button'),
+	nbBts = bts.length;
 	
-	init:function()
-};
-*/
+appPuissance4.init();
+for( var i = 0 ; i < nbBts ; i++ ) {
+	bts[i].addEventListener('click', appPuissance4.joue);
+}
